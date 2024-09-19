e025cf2cdd36026e93bea1a47c00e3b07dd7ae0d
Azzam Maulana Dzakwan <azzamdzakwan088@gmail.com>
2024-09-19 21:11:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var btnLove = document.getElementById('btn');
+
+function Toggle1(){
+    if (btnLove.style.color == "red") {
+        btnLove.style.color = "grey";
+    } else {
+        btnLove.style.color = "red";
+    }
+}
+
